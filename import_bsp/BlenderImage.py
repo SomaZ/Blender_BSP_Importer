a75d908b12978762e45a008e64b7714c5eb457a2
SomaZ <17459161+SomaZ@users.noreply.github.com>
2024-03-29 22:47:12 +0100

    Load images from DDS path in base too

--- import_bsp/BlenderImage.py
+++ import_bsp/BlenderImage.py
@@ -58,11 +58,9 @@ def load_file(file_path, VFS):
             continue
 
     # try loading ftx image
-    for fp in file_paths:
-        try:
-            image_bytearray = VFS.get(file_path_without_ext + ".ftx")
-            if image_bytearray is None:
-                continue
+    try:
+        image_bytearray = VFS.get(file_path_without_ext + ".ftx")
+        if image_bytearray is not None:
             image = loadFtx_from_bytearray(file_path_without_ext, image_bytearray)
             new_image = bpy.data.images.new(
                 image.name,
@@ -71,10 +69,29 @@ def load_file(file_path, VFS):
                 alpha=True)
             new_image.pixels = image.get_rgba()
             new_image.alpha_mode = 'CHANNEL_PACKED'
-            new_image.use_fake_user = True
+            new_image.pack()
             return new_image
-        except Exception:
-            continue
+    except Exception:
+        pass
+
+    # try loading DDS folder dds files
+    try:
+        image_bytearray = VFS.get("DDS/" + file_path_without_ext + ".dds")
+        if image_bytearray is not None:
+            temp_filename = bpy.app.tempdir + "DDS/" + file_path_without_ext + extension
+
+            os.makedirs(os.path.dirname(temp_filename), exist_ok=True)
+
+            temp_file = open(temp_filename, "wb")
+            temp_file.write(image_bytearray)
+            temp_file.close()
+
+            b_image = bpy.data.images.load(
+                temp_filename, check_existing=True)
+            b_image.pack()
+            return b_image
+    except Exception:
+        pass
 
     print("Couldn't load texture: ", file_path)
     return None
